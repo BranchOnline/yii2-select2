515994ecfa4cdceacad20f9db8d4799866d83397
Jap Mul Eindbaas <jap@branchonline.nl>
2015-01-27 14:21:30 +0100

    Initial commit

---
+++ widget/Select2Asset.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace branchonline\widget;
+
+use yii\web\AssetBundle;
+
+/**
+ * Description of FeedbackAsset
+ *
+ * @author jap
+ */
+class Select2Asset extends AssetBundle {
+
+    /**
+     * Set up CSS and JS asset arrays based on the base-file names
+     *
+     * @param string $type whether 'css' or 'js'
+     * @param array $files the list of 'css' or 'js' basefile names
+     */
+    protected function setupAssets($type, $files = []) {
+        $srcFiles = [];
+        foreach ($files as $file) {
+            $srcFiles[] = "{$file}.{$type}";
+        }
+        
+        if (empty($this->$type)) {
+            $this->$type = $srcFiles;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function init() {
+        $this->setSourcePath(__DIR__ . '/../vendor/ivaynberg/select2/');
+        $this->setupAssets('css', ['select2']);
+        $this->setupAssets('js', ['select2']);
+        parent::init();
+    }
+
+    /**
+     * Sets the source path if empty
+     * @param string $path the path to be set
+     */
+    protected function setSourcePath($path) {
+        if (empty($this->sourcePath)) {
+            $this->sourcePath = $path;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+}
